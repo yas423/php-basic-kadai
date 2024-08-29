0886a9a17c9e12587812b9dcb2e9faa58ec0fcfb
yas423 <yuasa@hodumi.com>
2024-08-29 12:58:22 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>課題007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+        // 連想配列に値を代入する
+        $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+ 
+        // 連想配列の値を出力する
+        print_r($Array);
+    ?>
+  </p>
+</body>
